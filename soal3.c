3974ea9033f688d15f7d1fd543b90903c2856cca
AgungDw <pif.zwei@gmail.com>
2017-10-24 21:13:26 +0700

    fungsi game

--- soal3.c
+++ soal3.c
@@ -2,6 +2,8 @@
 
 int kepiting_status, lohan_status;
 
+int status_game; //signal to determine the game. 0 if the game was end
+
 void kepiting_kelaparan()
 {
 	sleep (20);
@@ -9,6 +11,7 @@ void kepiting_kelaparan()
 	if (kepiting_status<0)
 	{
 		//give signal to end game
+		status_game=0;
 	}
 }
 
@@ -19,6 +22,33 @@ void lohan_kelaparan()
 	if (lohan_kelaparan<0)
 	{
 		//give signal to end game
+		status_game=0;
+	}
+}
+
+void game()
+{
+	int menu;
+	while (status_game==1)
+	{
+		printf("Menu:\n 1 untuk memberi makan lohan\n 2 untuk memberi makan kepiting");
+		scanf("%d", &menu);
+		if (menu==1)
+		{
+			lohan_status+=10;
+			if (lohan_status>100)
+			{
+				status_game=0;
+			}
+		}
+		if else (menu==2)
+		{
+			kepiting_status+=10;
+			if (kepiting_status>100)
+			{
+				status_game=0;
+			}
+		}
 	}
 }
 
@@ -26,5 +56,6 @@ void lohan_kelaparan()
 int main()
 {
 	status_lohan=status_kepiting=100;
+	status_game=1;
 
 }
