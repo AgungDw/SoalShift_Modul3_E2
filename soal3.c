0a924dd25e1479e7685e6c3823a6dfda91e8da7d
AgungDw <pif.zwei@gmail.com>
2017-10-24 21:00:43 +0700

    fungsi kepiting

---
+++ soal3.c
@@ -0,0 +1,20 @@
+#include <stdlib>
+
+int kepiting_status, lohan_status;
+
+void kepiting_kelaparan()
+{
+	sleep (20);
+	kepiting_status-=10;
+	if (kepiting_status<10)
+	{
+		//give signal to end game
+	}
+}
+
+
+int main()
+{
+	status_lohan=status_kepiting=100;
+
+}
