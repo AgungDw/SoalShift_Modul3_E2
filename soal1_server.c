9e044cd0acb2b72833d6dcff5fbf0a8a4d8059c1
AgungDw <pif.zwei@gmail.com>
2017-10-25 11:04:04 +0700

    debug pointer and data type

--- soal1_server.c
+++ soal1_server.c
@@ -5,7 +5,8 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 
 int main ()
@@ -15,6 +16,7 @@ int main ()
     int *weapon_ammount[6];
     int itterator;
     int input_amm;
+    int opt;
     for (itterator=0; itterator<6;itterator++)
     {
         int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
@@ -30,8 +32,12 @@ int main ()
     strcpy(weapon_name[3], "SS2-V5");
     strcpy(weapon_name[4], "SPG1-V3");
     strcpy(weapon_name[5], "MINE");
-    &weapon_ammount[0]=&weapon_ammount[1]=&weapon_ammount[2]=
-    &weapon_ammount[3]=&weapon_ammount[4]=&weapon_ammount[5]=0;
+    *weapon_ammount[0]=0;
+    *weapon_ammount[1]=0;
+    *weapon_ammount[2]=0;
+    *weapon_ammount[3]=0;
+    *weapon_ammount[4]=0;
+    *weapon_ammount[5]=0;
 
     while (1)
     {
@@ -41,17 +47,17 @@ int main ()
         {
             for (itterator=0; itterator<6; itterator++)
             {
-                if (&weapon_ammount[itterator]>0) printf("%s %li\n", weapon_name[itterator], weapon_ammount[itterator]);
+                if (&weapon_ammount[itterator]>0) printf("%s %d\n", weapon_name[itterator], *weapon_ammount[itterator]);
             }
         }
         else if (opt==2)
         {
-            scanf("%s %li", input,&input_amm);
+            scanf("%s %d", input,&input_amm);
             for (itterator=0; itterator<6; itterator++)
             {
                 if (strcmp (weapon_name[itterator],input)==0)
                 {
-                    weapon_ammount[itterator]+=input_amm;
+                    *weapon_ammount[itterator]+=input_amm;
                 }
             }
         }
