f4fe8c39339d11fc2c8dc0ec48cc543b105970d4
AgungDw <pif.zwei@gmail.com>
2017-10-25 07:49:50 +0700

    created global variable and menu

--- soal1_server.c
+++ soal1_server.c
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #define PORT 8080
 
+int weapon_ammount[6];
+char weapon_name [6][10];
+
 void listener(void * arg);
 
 
@@ -16,9 +19,16 @@ int main ()
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char input[50];
-    
-
+    char input[10];
+    //MP4A1, PM2-V1, SPR-3, SS2-V5, SPG1-V3,​ ​ dan​​ ​ MINE
+    strcpy(weapon_name[0], "MP4A1");
+    strcpy(weapon_name[1], "PM2-V1");
+    strcpy(weapon_name[2], "SPR-3");
+    strcpy(weapon_name[3], "SS2-V5");
+    strcpy(weapon_name[4], "SPG1-V3");
+    strcpy(weapon_name[5], "MINE");
+    weapon_ammount[0]=weapon_ammount[1]=weapon_ammount[2]=
+    weapon_ammount[3]=weapon_ammount[4]=weapon_ammount[5]=0;
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
@@ -57,10 +67,33 @@ int main ()
 	}
 
     int opt;
+    int itterator;
     while (1)
     {
     	printf("1 untuk lihat stok senjata\n2 untuk tambah stok senjata");	
-    	
+    	scanf("%d", &opt);
+        if (opt==1)
+        {
+            for (itterator=0; itterator<6; itterator++)
+            {
+                printf("%s %d\n", weapon_name[itterator], weapon_ammount[itterator]);
+            }
+        }
+        else if (opt==2)
+        {
+            scanf("%s %d", input, opt);
+            for (itterator=0; itterator<6; itterator++)
+            {
+                if (strcmp (weapon_name[itterator],input)==0)
+                {
+                    weapon_ammount[itterator]+=opt;
+                }
+            }
+        }
+        else 
+        {
+            input salah;
+        }
     }
     
     return 0;
