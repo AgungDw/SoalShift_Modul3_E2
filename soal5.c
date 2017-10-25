22d4ee3fdde93be5008a5f59a797fb7d33d39663
AgungDw <pif.zwei@gmail.com>
2017-10-25 13:26:46 +0700

    nomor 5 main

--- soal5.c
+++ soal5.c
@@ -1,39 +1,42 @@
-#include<stdio.h>
-#include<string.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
 
-int main (void)
+int main ()
 {
-	
-	char input[256];
-	gets(input);
-	if (strncmp(input, "./cari", 5));
-	int itterator;
-	itterator= strlen(input);
-	int leng=itterator;
-	int temp=leng;
-	char passer [50];
-	int i=0;
-	pthread_t tid[10];
-	for (itterator-=1; itterator>5;itterator--)
-	{
-		if(input[itterator] == ' '&&itterator!=leng)
-		{
-			int x=itterator+1;
-			int y=0;
-			for (x; x<temp; x++)
-			{
-				passer[y]=input[x];
-				y++;
-			}
-			passer[y]='\0';
-			temp=itterator;
-			//printf("%s\n", passer);
-			pthread_create(&tid[i], NULL, finder, (void *)x);
-			i++;
+	memset(jumlah,0,500);
+	int k=0,l=0,len,err,n[1000];
+	char trap[11],input[1000];
+	book=fopen("Novel.txt","r");
+	scanf("%s",&trap);
+	getchar();
+	scanf("%[^\n]s",&input);
+	getchar();
+	fscanf(book,"%[^\n]s",&baris);
+	len=strlen(input);
+	for(i=0;i<len;i++){
+		if(input[i]==' '){
+			++j;
+			k=0;
+			++l;
+		}
+		else{
+			temp[j][k]=input[i];
+			++k;
 		}
 	}
+	for(i=0;i<=j;i++){
+		err=pthread_create(&(tid[i]),NULL,&cari_nama,NULL);
+		if(err!=0){
+			printf("Program error. Please restart the program\n");
+			return -1;
+		}
+		while(selesai<=i);
+	}
+	for(i=0;i<=j;i++){
+		printf("%s : %d\n",temp[i],jumlah[i]);
+	}
+	return 0;
+}
 }
